9c3a05acc51bb6ae9facc3d4b85160d4183449db
Luna Razzaghipour <aramisnoah@gmail.com>
2022-07-27 11:36:39 +1000

    Add missing syntax tree types

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -1,6 +1,19 @@
 use std::mem;
 
+pub type SyntaxTree = eventree::SyntaxTree<TreeConfig>;
+pub type SyntaxBuilder = eventree::SyntaxBuilder<TreeConfig>;
+pub type SyntaxNode = eventree::SyntaxNode<TreeConfig>;
+pub type SyntaxToken = eventree::SyntaxToken<TreeConfig>;
+pub type SyntaxElement = eventree::SyntaxElement<TreeConfig>;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u8)]
+pub enum NodeKind {
+	SourceFile,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u8)]
 pub enum TokenKind {
 	StructKw,
 	Ident,
@@ -11,6 +24,19 @@ pub enum TokenKind {
 	Error,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TreeConfig {}
+
+unsafe impl eventree::SyntaxKind for NodeKind {
+	fn to_raw(self) -> u16 {
+		self as u16
+	}
+
+	unsafe fn from_raw(raw: u16) -> Self {
+		mem::transmute(raw as u8)
+	}
+}
+
 unsafe impl eventree::SyntaxKind for TokenKind {
 	fn to_raw(self) -> u16 {
 		self as u16
@@ -19,4 +45,9 @@ unsafe impl eventree::SyntaxKind for TokenKind {
 	unsafe fn from_raw(raw: u16) -> Self {
 		mem::transmute(raw as u8)
 	}
+}
+
+impl eventree::TreeConfig for TreeConfig {
+	type NodeKind = NodeKind;
+	type TokenKind = TokenKind;
 }
