d92bf90fa3a66377043aa9b8d707d31c6e9a9b1f
Luna Razzaghipour <aramisnoah@gmail.com>
2022-07-30 17:49:28 +1000

    Parse all `*.fd` files in the current directory

--- crates/compiler/src/main.rs
+++ crates/compiler/src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-	println!("Hello, world!");
+use std::ffi::OsStr;
+use std::fs;
+use text_size::TextSize;
+
+fn main() -> anyhow::Result<()> {
+	for entry in fs::read_dir(".")? {
+		let path = entry?.path();
+		if path.extension() != Some(OsStr::new("fd")) {
+			continue;
+		}
+		let file_name = path.file_name().unwrap().to_str().unwrap();
+
+		let content = fs::read_to_string(&path)?;
+		let line_starts = line_starts(&content);
+
+		let parse = parser::parse(&content);
+		for error in parse.errors {
+			let (offset, message) = match error {
+				parser::Error::Expected { range, message } => {
+					(range.start(), format!("expected {message}"))
+				}
+				parser::Error::Missing { offset, message } => {
+					(offset, format!("missing {message}"))
+				}
+			};
+			let (line, column) = offset_to_line_column(offset, &line_starts);
+			println!(
+				"{file_name}:{}:{}: error: {message}",
+				line + 1,
+				column + 1
+			);
+		}
+
+		let _header = ast::gen_header(&parse.tree);
+	}
+
+	Ok(())
+}
+
+fn offset_to_line_column(offset: TextSize, line_starts: &[u32]) -> (u32, u32) {
+	let offset = u32::from(offset);
+	let line = line_starts.partition_point(|&i| i <= offset) - 1;
+	let column = offset - line_starts[line];
+	(line as u32, column)
+}
+
+fn line_starts(input: &str) -> Vec<u32> {
+	let mut line_starts = vec![0];
+	for (i, _) in input.match_indices('\n') {
+		line_starts.push(i as u32 + 1);
+	}
+	line_starts
 }
