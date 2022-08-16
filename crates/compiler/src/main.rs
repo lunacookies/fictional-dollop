8278c10d27d9d85a188db22a3f806a82a6632b33
Luna Razzaghipour <aramisnoah@gmail.com>
2022-08-17 09:31:30 +1000

    Codegen from CLI

--- crates/compiler/src/main.rs
+++ crates/compiler/src/main.rs
@@ -7,7 +7,13 @@ fn main() -> anyhow::Result<()> {
 	let files = read_files()?;
 	let parses = parse(&files);
 	let raw_index = index(&parses);
-	let _resolved_index = resolve(raw_index, &files);
+	let resolved_index = resolve(raw_index, &files);
+	let hirs = lower(&resolved_index, &files, &parses);
+	let mirs = build_mirs(&hirs);
+	let asm = codegen(&mirs, &resolved_index);
+
+	println!("{asm}");
+
 	Ok(())
 }
 
@@ -107,6 +113,73 @@ fn resolve(
 	}
 
 	resolved_index
+}
+
+fn lower(
+	index: &resolved_index::Index,
+	files: &HashMap<String, (String, Vec<u32>)>,
+	parses: &HashMap<String, (cst::SourceFile, syntax::SyntaxTree)>,
+) -> HashMap<String, hir::Hir> {
+	let mut hirs = HashMap::new();
+
+	for (name, stub) in &index.stubs {
+		let (source_file, tree) = &parses[name];
+		let (hir, errors) = hir::lower(stub, *source_file, tree);
+		hirs.insert(name.clone(), hir);
+
+		let (content, line_starts) = &files[name];
+		for error in errors {
+			let message = match error.kind {
+				hir::ErrorKind::UndefinedVariable => {
+					format!("undefined variable `{}`", &content[error.range])
+				}
+			};
+			let (line, column) =
+				offset_to_line_column(error.range.start(), line_starts);
+			println!(
+				"{name}.fd:{}:{}: error: {message}",
+				line + 1,
+				column + 1
+			);
+		}
+	}
+
+	hirs
+}
+
+fn build_mirs(hirs: &HashMap<String, hir::Hir>) -> HashMap<String, mir::Mir> {
+	let mut mirs = HashMap::new();
+	for (name, hir) in hirs {
+		let mir = mir::build_mir(hir);
+		mirs.insert(name.clone(), mir);
+	}
+	mirs
+}
+
+fn codegen(
+	mirs: &HashMap<String, mir::Mir>,
+	index: &resolved_index::Index,
+) -> String {
+	let mut asm = String::new();
+
+	for (i, (name, mir)) in mirs.iter().enumerate() {
+		if i != 0 {
+			asm.push('\n');
+		}
+
+		let stub = &index.stubs[name];
+		for (i, (_function, a)) in
+			codegen_aarch64::codegen(mir, stub, name).into_iter().enumerate()
+		{
+			if i != 0 {
+				asm.push('\n');
+			}
+
+			asm.push_str(&a);
+		}
+	}
+
+	asm
 }
 
 fn offset_to_line_column(offset: TextSize, line_starts: &[u32]) -> (u32, u32) {
